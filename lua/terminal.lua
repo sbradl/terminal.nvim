b27fd1cec693c0f3ab9ab48720ff98bc93121214
Stefan Bradl <bradlstefan@googlemail.com>
2026-07-09 21:31:28 +0200

    allow commands to be configured via setup

--- lua/terminal.lua
+++ lua/terminal.lua
@@ -3,27 +3,36 @@ local M = {}
 local t = require("terminal_handling")
 local c = require("commands")
 
-local filetype_commands = {}
+M._filetype_commands = {}
 
-local function register(ext, command_list)
-	if not filetype_commands[ext] then
-		filetype_commands[ext] = {}
+M.register = function(ext, command_list)
+	if not M._filetype_commands[ext] then
+		M._filetype_commands[ext] = {}
 	end
 
 	for _, cmd in ipairs(command_list) do
-		table.insert(filetype_commands[ext], cmd)
+		table.insert(M._filetype_commands[ext], cmd)
 	end
 end
 
-register("ts", require("vitest").commands)
-register("ts", require("playwright").commands)
-register("cs", require("dotnet_test").commands)
+M.register("ts", require("vitest").commands)
+M.register("ts", require("playwright").commands)
+M.register("cs", require("dotnet_test").commands)
 
-M.setup = function() end
+M.setup = function(opts)
+	opts = opts or {}
+
+	if opts.commands then
+		for ext, command_list in pairs(opts.commands) do
+			M.register(ext, command_list)
+		end
+	end
+end
+
 M.focus_last_terminal = t.focus_last_terminal
 M.open_new_terminal = t.open_new_terminal
 M.run_command = function()
-	c.choose_and_run_command(filetype_commands)
+	c.choose_and_run_command(M._filetype_commands)
 end
 
 return M
